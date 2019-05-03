2c5946cbdebe61f473825ea56b28cb6df97a6c0a
yvan vander sanden <yvan@attr-x.net>
2019-05-03 15:28:04 +0200

    fixed wisa web reference

--- WisaApi/Connector.cs
+++ WisaApi/Connector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace WisaApi
         public static Dictionary<string, string> ReplaceInstNumber = new Dictionary<string, string>();
 
         private static WISA.TWISAAPICredentials credentials;
-        private static WISA.WisaAPIService service;
+        private static WISA.WisaAPIServiceService service;
         
         internal static ILog Log;
 
@@ -24,21 +25,48 @@ namespace WisaApi
             credentials.Password = password;
             credentials.Database = database;
 
-            service = new WISA.WisaAPIServiceClient("Endpoint", "http://" + url + ":" + port.ToString() + "/SOAP/");
+            service = new WISA.WisaAPIServiceService();
+            service.Url = "http://" + url + ":" + port.ToString() + "/SOAP/";
         }
 
         static public async Task<string> PerformQuery(string name, WISA.TWISAAPIParamValue[] values)
         {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    var data = service.GetCSVData(credentials, name, values, true, ",", "");
+                    return Encoding.Default.GetString(data);
+                }
+                catch (Exception e)
+                {
+                    Log?.Add("Wisa Query: " + e.Message, true);
+                    return String.Empty;
+                }
+            });
+        }
+
+        static public void GetClassList(string IDListCSV)
+        {
+            List<WISA.TWISAAPIParamValue> values = new List<WISA.TWISAAPIParamValue>();
+            values.Add(new WISA.TWISAAPIParamValue());
+            values.Last().Name = "IS_ID";
+            values.Last().Value = IDListCSV;
+            values.Add(new WISA.TWISAAPIParamValue());
+            values.Last().Name = "Werkdatum";
+            values.Last().Value = "22/05/2018";
+
             try
             {
-                var data = await service.GetCSVDataAsync(new WISA.GetCSVDataRequest(credentials, name, values, true, ",", ""));
-                return Encoding.Default.GetString(data.Result);
-                
-            } catch(Exception e)
+                var result = service.GetCSVData(credentials, "SMATestQ", values.ToArray(), true, ",", "");
+                var str = System.Text.Encoding.Default.GetString(result);
+                Debug.Write(str);
+            }
+            catch (Exception e)
             {
-                Log?.Add("Wisa Query: " + e.Message, true);
-                return String.Empty;
+                Debug.WriteLine(e.Message);
             }
         }
     }
 }
+
--- WisaTest/WisaConnector.cs
+++ WisaTest/WisaConnector.cs
@@ -1,19 +1,127 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WisaTest
 {
+    class Log : WisaApi.ILog
+    {
+        public void Add(string message, bool error = false)
+        {
+            string output = error ? "Error: " : "Message: ";
+            output += message;
+
+            Debug.WriteLine(output);
+        }
+    }
+
     [TestClass]
     public class WisaConnector
     {
         public WisaConnector()
         {
-            
+            WisaApi.Connector.Init(
+                Properties.Settings.Default.WisaUrl,
+                Properties.Settings.Default.WisaPort,
+                Properties.Settings.Default.WisaAccount,
+                Properties.Settings.Default.WisaPassword,
+                Properties.Settings.Default.WisaDatabase,
+                new Log()
+            );
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void LoadClasses()
         {
+           
+        }
+
+        [TestMethod]
+        public async Task LoadSchools()
+        {
+            bool result = await WisaApi.Schools.Load();
+            Assert.IsTrue(result);
+            Assert.IsTrue(WisaApi.Schools.All.Count > 0);
+
+            // load again to see if the previous list is cleared
+            int count = WisaApi.Schools.All.Count;
+            result = await WisaApi.Schools.Load();
+            Assert.IsTrue(result);
+            Assert.IsTrue(WisaApi.Schools.All.Count == count);
+
+            // set some schools to active
+            bool value = true;
+            foreach (var school in WisaApi.Schools.All)
+            {
+                school.IsActive = value;
+                value = !value;
+            }
+
+            string list = WisaApi.Schools.ActiveSchoolsToString();
+            Assert.IsTrue(list.Length > 0);
+
+            Assert.IsTrue(WisaApi.Schools.ActiveSchoolsFromString(list));
+
+            // odd entries should still be false
+            value = true;
+            foreach (var school in WisaApi.Schools.All)
+            {
+                Assert.IsTrue(school.IsActive == value);
+                value = !value;
+            }
+        }
+
+        [TestMethod]
+        public async Task LoadStudents()
+        {
+            WisaApi.School school = new WisaApi.School(
+              25,
+              "TEST",
+              "Does not matter"
+            );
+
+            bool result = await WisaApi.Students.Add(school);
+            Assert.IsTrue(result);
+            Assert.IsTrue(WisaApi.Students.All.Count > 0);
+
+            WisaApi.Students.Clear();
+            Assert.IsTrue(WisaApi.Students.All.Count == 0);
+        }
+
+        [TestMethod]
+        public async Task LoadStaff()
+        {
+            WisaApi.School school = new WisaApi.School(
+             25,
+             "TEST",
+             "Does not matter"
+            );
+
+            bool result = await WisaApi.StaffMembers.Add(school);
+            Assert.IsTrue(result, "StaffMembers.Add should return True");
+            Assert.IsTrue(WisaApi.StaffMembers.All.Count > 0, "StaffMembers.All should contain data");
+
+            WisaApi.StaffMembers.Clear();
+            Assert.IsTrue(WisaApi.StaffMembers.All.Count == 0, "StaffMembers.All should be empty");
+        }
+
+        [TestMethod]
+        public async Task LoadClassGroups()
+        {
+            WisaApi.School school = new WisaApi.School(
+              25,
+              "TEST",
+              "Does not matter"
+            );
+
+            WisaApi.ClassGroups.Clear();
+            bool result = await WisaApi.ClassGroups.Add(school);
+            Assert.IsTrue(result);
+            Assert.IsTrue(WisaApi.ClassGroups.All.Count > 0);
+
+            WisaApi.ClassGroups.Clear();
+            Assert.IsTrue(WisaApi.ClassGroups.All.Count == 0);
         }
     }
 }
